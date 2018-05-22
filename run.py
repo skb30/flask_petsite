15820563fe8408cc9e88d26ae7733a8d2dc3dcb1
Scott K. Barth <sbarth@gmail.com>
2018-05-22 11:15:50 -0700

    fixed error in run.

--- run.py
+++ run.py
@@ -3,19 +3,19 @@ from app.auth.models import User
 
 #if __name__ == '__main__':
 
-    # use our app factory to create the app
-    flask_app = create_app('prod')
+# use our app factory to create the app
+flask_app = create_app('prod')
 
 
-    # tell flask to use the current app. remember with this scalable arch
-    # we can run several differnt instances of this app
-    with flask_app.app_context():
-        db.create_all()
+# tell flask to use the current app. remember with this scalable arch
+# we can run several differnt instances of this app
+with flask_app.app_context():
+    db.create_all()
 
-        # if user doesn't exist in db, then create it.
-        if not User.query.filter_by(user_name='harry').first():
-            # class method defined in models.py
-            User.create_user(user='harry',
-                             email='harry@potters.com',
-                            password='secret')
-        flask_app.run()
+    # if user doesn't exist in db, then create it.
+    if not User.query.filter_by(user_name='harry').first():
+        # class method defined in models.py
+        User.create_user(user='harry',
+                         email='harry@potters.com',
+                        password='secret')
+    flask_app.run()
