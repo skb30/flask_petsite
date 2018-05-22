37dfa138f378e7fd5bf25687e0cf83b6bd95d869
Scott K. Barth <sbarth@gmail.com>
2018-05-22 12:29:33 -0700

    added error checking

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from app.auth.models import User
+from sqlalchemy import exc
 
 #if __name__ == '__main__':
 
@@ -12,10 +13,13 @@ flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
 
-    # if user doesn't exist in db, then create it.
-    if not User.query.filter_by(user_name='harry').first():
-        # class method defined in models.py
-        User.create_user(user='harry',
-                         email='harry@potters.com',
-                        password='secret')
-    flask_app.run()
+    try:
+
+        # if user doesn't exist in db, then create it.
+        if not User.query.filter_by(user_name='harry').first():
+            # class method defined in models.py
+            User.create_user(user='harry',
+                             email='harry@potters.com',
+                            password='secret')
+    except exc.IntegrityError:
+        flask_app.run()
