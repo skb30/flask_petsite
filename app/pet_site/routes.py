f9ef31d3fec46b5dbff31cd1a803953421df52cf
Scott K. Barth <sbarth@gmail.com>
2018-06-18 16:00:00 -0700

    error correction

--- app/pet_site/routes.py
+++ app/pet_site/routes.py
@@ -8,7 +8,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from flask import render_template, flash, request, redirect, url_for
 from app.pet_site.forms import CreatePetForm, EditPetForm
 from datetime import date
-
+@pets.route('/')
 @pets.route('/pet_site')
 def display_home():
     return render_template('home.html')
